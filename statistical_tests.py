74746ce045f96aed536754b90b7085b5d235e409
lina-aa <karolin.bajer@gmail.com>
2025-05-19 09:55:48 +0200

    testing update

--- statistical_tests.py
+++ statistical_tests.py
@@ -3,6 +3,66 @@ from tabulate import tabulate
 
 from scipy.stats import shapiro, ttest_rel, wilcoxon
 
+def print_scores(classifier_name, oversampling_name, rounding=None, table_style="grid", T=False):
+    M_list = [25, 50, 75, 100]
+    lr_list = [0.1, 0.5, 1, 10]
+    alpha_list = [round(0.1 * i,1) for i in range(1, 11)]
+
+    # A list of the parameters permutation - models names, used as column headers.
+    if oversampling_name.lower() == "vao":
+        if classifier_name.lower() == "ab" or classifier_name.lower() == "sab":
+            model_names =  [f"{x}-{y}-{z}" for x in M_list for y in lr_list for z in alpha_list]
+        else:
+            model_names = [f"{x}-{y}" for x in M_list for y in alpha_list]
+    else:
+        if classifier_name.lower() == "ab" or classifier_name.lower() == "sab":
+            model_names = [f"{x}-{y}" for x in M_list for y in lr_list]
+        else:
+            model_names = M_list
+
+
+    metrics = ["Precision", "Recall", "F1 score", "G-mean"]
+
+    pre_scores = np.load(f"wyniki/{classifier_name.lower()}_{oversampling_name.lower()}_precision.npy")
+    rec_scores = np.load(f"wyniki/{classifier_name.lower()}_{oversampling_name.lower()}_recall.npy")
+    f1_scores = np.load(f"wyniki/{classifier_name.lower()}_{oversampling_name.lower()}_f1_score.npy")
+    gm_scores = np.load(f"wyniki/{classifier_name.lower()}_{oversampling_name.lower()}_g-mean.npy")
+
+    scr = {"Precision":pre_scores, "Recall":rec_scores, "F1 score":f1_scores, "G-mean":gm_scores}
+    mean_scores = []
+    std_scores = []
+    for s in scr.keys():
+        if rounding != None:
+            mean_scores.append(np.round(np.mean(scr[s], axis=1), rounding))
+            std_scores.append(np.round(np.std(scr[s], axis=1), rounding))
+        else:
+            mean_scores.append(np.mean(scr[s], axis=1))
+            std_scores.append(np.std(scr[s], axis=1))
+
+    mean_scores = np.array(mean_scores)
+    std_scores = np.array(std_scores)
+    scores = np.char.add(np.char.add(mean_scores.astype(str), u' \u00B1 '), std_scores.astype(str))
+    if T == True:
+        scores_T = scores.T
+        table = tabulate(scores_T, 
+                        tablefmt=table_style, 
+                        headers=metrics, 
+                        showindex=model_names
+        )
+    elif T == False:
+        table = tabulate(scores, 
+                        tablefmt=table_style, 
+                        headers=model_names, 
+                        showindex=metrics
+        )
+
+    if table_style == "grid":
+        print(f"\n", f"Scores for {classifier_name} classifiers with {oversampling_name} oversampling")
+        print(table)
+    else:
+        table_latex = table[:-13] + f"\caption{{Scores for {classifier_name} classifier with {oversampling_name} oversampling}}\n" + table[-13:]
+        print(table_latex, "\n")
+
 
 def tables_one_param(method_name, param_list, table_style):
 
@@ -102,13 +162,19 @@ def tables_three_params(method_name, first_param_list, second_param_list, third_
         print(table)
 
 
-M_list = [25, 50, 75, 100]
-lr_list = [0.1, 0.5, 1, 10]
-alpha_list=[0.1 * i for i in range(1, 10)]
+# M_list = [25, 50, 75, 100]
+# lr_list = [0.1, 0.5, 1, 10]
+# alpha_list = [round(0.1 * i,1) for i in range(1, 11)]
+# oversamplings = ["NONE", "SMOTE", "RUS", "VAO"]
+# classifiers = ["SWSEL", "RF", "AB", "SAB"]
+
+# method_name = f"{clf_name}_{sampling}"
 # tables_one_param("SWSEL", M_list, "grid")
 # tables_one_param("SMRF", M_list, "grid")
 # tables_two_params("SMAB",  M_list, lr_list, "grid")
 # tables_three_params("VASA", alpha_list, M_list, lr_list, "grid")
+
+print_scores("ab", "none", rounding=3, T=True)
 
 def compare_models(scores, model_names, table_style="grid", alpha=0.05, alternative="two-sided"):
     stat_matrix = [[None for _ in range(scores.shape[0])] for _ in range(scores.shape[0])]
@@ -128,21 +194,21 @@ def compare_models(scores, model_names, table_style="grid", alpha=0.05, alternat
                     headers=model_names, 
                     showindex=model_names)
     
-    print("\nSignificance Matrix (1 = significant difference, 0 = no significant difference):")
+    print("\n Matrix of p-values from paired statistical tests between models")
     print(table)
     return table
 
 #For comparing form .npy file, scores should be extracted from a file
-method_name = "rusab"
-metric = "Precision"
-alpha = 0.05
-alternative_hypothesis = "greater"
-scores = np.load(f"wyniki/{method_name.lower()}_{metric.lower()}.npy")
-if method_name == "VASA":
-    scores = scores[0]
+# method_name = "rusab"
+# metric = "Precision"
+# alpha = 0.05
+# alternative_hypothesis = "greater"
+# scores = np.load(f"wyniki/{method_name.lower()}_{metric.lower()}.npy")
+# if method_name == "VASA":
+    # scores = scores[0]
 
-vasa_names = [f"M {x}-lr {y}-a {z}" for x in M_list for y in lr_list for z in alpha_list]
-rusab_names = [f"M {x}- lr {y}" for x in M_list for y in lr_list]
-smrf_names = M_list
+# vasa_names = [f"M {x}-lr {y}-a {z}" for x in M_list for y in lr_list for z in alpha_list]
+# rusab_names = [f"M {x}- lr {y}" for x in M_list for y in lr_list]
+# smrf_names = M_list
 
-compare_models(scores, rusab_names, "latex")
+# compare_models(scores, rusab_names, "latex")
